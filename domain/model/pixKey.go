f5a3375ba0fb9920668e361e1b2bb34bd28d061b
Rômulo Valentim <valentim.canejo@hotmail.com>
2024-01-07 11:41:45 -0300

    FEAT: create GORM implementation.

--- domain/model/pixKey.go
+++ domain/model/pixKey.go
@@ -18,11 +18,11 @@ type PixKeyRepositoryInterface interface {
 
 type PixKey struct {
 	Base      `valid:"required"`
-	Kind      string   `json:"kind" valid:"notnull"`
-	Key       string   `json:"key" valid:"notnull"`
-	AccountID string   `json:"account_id" valid:"notnull"`
+	Kind      string   `json:"kind" gorm:"type:varchar(20)" valid:"notnull"`
+	Key       string   `json:"key" gorm:"type:varchar(255)" valid:"notnull"`
+	AccountID string   `gorm:"column:account_id;type:uuid;not null" valid:"-"`
 	Account   *Account `valid:"-"`
-	Status    string   `json:"status" valid:"notnull"`
+	Status    string   `json:"status" gorm:"type:varchar(20)" valid:"notnull"`
 }
 
 func (pixKey *PixKey) isValid() error {
